389d4520ac116453601ae8ded6008b57cda18626
Daniel Gent <dan@whalar.com>
2020-06-27 17:50:29 +0200

    high review and low review returned

--- api/routes/restaurants.js
+++ api/routes/restaurants.js
@@ -1,42 +1,61 @@
 var express = require("express");
 var router = express.Router();
-var _ = require("lodash");
 var RestaurantModel = require("../models/RestaurantModel");
 var ReviewsModel = require("../models/ReviewsModel");
+
+const enrichRestaurant = async (r) => {
+  const reviews = await ReviewsModel.find({
+    restaurant: r._id,
+  })
+    // not sure if this makes a difference
+    .lean()
+    .exec();
+
+  const total = reviews.length;
+
+  let sum = 0;
+  let highReview = null;
+  let lowReview = null;
+
+  for (let i = 0; i < total; i++) {
+    console.log("loop i ", i);
+    const r = reviews[i];
+    sum += r.rating;
+    if (!lowReview || r.rating <= lowReview.rating) {
+      lowReview = r;
+    }
+    if (!highReview || r.rating >= highReview.rating) {
+      highReview = r;
+    }
+  }
+
+  const averageRating = sum / total;
+
+  return {
+    // TODO - investigate this. Can't just spread in mongoose model
+    ...r._doc,
+    averageRating,
+    highReview,
+    lowReview,
+  };
+};
 
 router.get("/", async (req, res) => {
   // TODO - need these fields and this arg?
   const restaurants = await RestaurantModel.find({}, "name owner").exec();
 
-  const asyncRequests = restaurants.map(async (r) => {
-    const reviews = await ReviewsModel.find({
-      restaurant: r._id,
-    })
-      // not sure if this makes a difference
-      .lean()
-      .exec();
-
-    const total = reviews.length;
-
-    const sum = _.sumBy(reviews, "rating");
-
-    const averageRating = sum / total;
-
-    return {
-      // TODO - investigate this. Can't just spread in mongoose model
-      ...r._doc,
-      averageRating,
-    };
-  });
-
-  const enrichedRestaurantsPromise = Promise.all(asyncRequests);
-
-  const enrichedRestaurants = await enrichedRestaurantsPromise;
+  const enrichedRestaurants = await Promise.all(
+    restaurants.map(enrichRestaurant)
+  );
 
   res.status(200).send(enrichedRestaurants);
 });
 
 router.get("/:id", async (req, res) => {
+  // TODO derive fields:
+  // * averageRating: need helper function to do same as above? or copy for now
+  // * highReview: need to also loop through reviews and get this.
+  // * lowReview: need to also loop through reviews and get this.
   const id = req.params.id;
 
   RestaurantModel.findById(id, function (err, resMongo) {
