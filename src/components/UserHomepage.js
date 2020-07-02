38daed2d9aca9a11a7b27e08abbe9099807274cc
Daniel Gent <dan@whalar.com>
2020-07-02 17:07:29 +0200

    working filters with basic UX

--- src/components/UserHomepage.js
+++ src/components/UserHomepage.js
@@ -1,6 +1,12 @@
 import React from "react";
 import axios from "axios";
-import { Box, CircularProgress } from "@chakra-ui/core";
+import {
+  Box,
+  CircularProgress,
+  Select,
+  FormControl,
+  FormLabel,
+} from "@chakra-ui/core";
 
 import { getAuthHeader } from "utils";
 import RestaurantListItem from "components/RestaurantListItem";
@@ -8,11 +14,7 @@ import RestaurantListItem from "components/RestaurantListItem";
 const UserHomepage = (props) => {
   const [isLoading, setIsLoading] = React.useState(true);
   const [restaurants, setRestaurants] = React.useState([]);
-  const [
-    filters,
-    // TODO - filter by avg. rating
-    // setFilters
-  ] = React.useState({});
+  const [filters, setFilters] = React.useState({ min: 1, max: 5 });
 
   React.useEffect(() => {
     setIsLoading(true);
@@ -29,12 +31,57 @@ const UserHomepage = (props) => {
       });
   }, [filters]);
 
+  const handleMinRatingChange = (e) => {
+    const min = e.target.value;
+    let max = filters.max;
+    if (min > max) {
+      max = min;
+    }
+    setFilters({ max, min });
+  };
+
+  const handleMaxRatingChange = (e) => {
+    const max = e.target.value;
+
+    let min = filters.min;
+    if (max < min) {
+      min = max;
+    }
+    setFilters({ max, min });
+  };
+
   if (isLoading) {
     return <CircularProgress isIndeterminate color="green"></CircularProgress>;
   }
 
   return (
     <Box p={4}>
+      <Box p={4}>
+        <FormControl>
+          <FormLabel htmlFor="username">
+            Select minimum average rating
+          </FormLabel>
+          <Select value={filters.min} onChange={handleMinRatingChange}>
+            <option value={1}>1</option>
+            <option value={2}>2</option>
+            <option value={3}>3</option>
+            <option value={4}>4</option>
+            <option value={5}>5</option>
+          </Select>
+        </FormControl>
+        <FormControl>
+          <FormLabel htmlFor="username">
+            Select maximum average rating
+          </FormLabel>
+          <Select value={filters.max} onChange={handleMaxRatingChange}>
+            <option value={1}>1</option>
+            <option value={2}>2</option>
+            <option value={3}>3</option>
+            <option value={4}>4</option>
+            <option value={5}>5</option>
+          </Select>
+        </FormControl>
+      </Box>
       {restaurants.map((restaurant) => (
         <RestaurantListItem key={restaurant._id} restaurant={restaurant} />
       ))}
